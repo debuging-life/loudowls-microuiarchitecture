172b1b5bb983066a2a500c09987042f2b6ac3e16
Pardip Bhatti <pardipbhatti28@gmail.com>
2026-04-12 16:51:19 -0400

    Initial Commit

---
+++ micruiachitecture/micruiachitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  micruiachitectureApp.swift
+//  micruiachitecture
+//
+//  Created by Pardip Bhatti on 12/04/26.
+//
+
+import SwiftUI
+
+@main
+struct micruiachitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
